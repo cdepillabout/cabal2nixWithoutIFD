4a7f4d8b26fe152f7c44baf8474ecff4bc26ea4f
Jonas Carpay <jonascarpay@gmail.com>
2021-09-25 18:41:55 +0900

    Update purenix/src/Nix/Convert.hs
    Co-authored-by: Dennis Gosnell <cdep.illabout@gmail.com>

--- purenix/src/Nix/Convert.hs
+++ purenix/src/Nix/Convert.hs
@@ -94,7 +94,7 @@ expr (Var ann (P.Qualified mqual name)) = localAnn ann $ do
     Just qual
       | qual /= thisModule -> N.sel (N.sel (N.var "module") (N.moduleKey qual)) (N.identKey name)
     _ -> N.var (N.mkVar name)
-expr (Accessor ann sel body) = localAnn ann $ flip N.sel (stringToKey sel) <$> expr body
+expr (Accessor ann sel body) = localAnn ann $ flip N.sel (N.stringKey sel) <$> expr body
 expr (Let ann binds body) = localAnn ann $ liftA2 N.let' (bindings binds) (expr body)
 expr (ObjectUpdate ann a b) = localAnn ann $ liftA2 (N.bin N.Update) (expr a) (attrs b)
 expr (Constructor _ _ (P.ProperName dataName) fields) = pure $ N.constructor dataName (N.mkVar <$> fields)
