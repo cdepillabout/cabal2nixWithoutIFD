6bc09066254d59575a9e4de00b9e3525b8bd31db
(cdep)illabout <cdep.illabout@gmail.com>
2021-10-10 12:01:58 +0900

    Update flake.nix and packages.dhall for our local packages

--- flake.nix
+++ flake.nix
@@ -1,61 +1,24 @@
 {
-  description = "purenix";
+  description = "cabal2nixWithoutIFD";
 
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.purenix.url = "github:purenix-org/purenix";
+  inputs.nixpkgs.follows = "purenix/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+  outputs = { self, nixpkgs, flake-utils, purenix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlay = self: _: {
-          hsPkgs =
-            self.haskell-nix.project' rec {
-              projectFileName = "stack.yaml";
-              src = ./purenix;
-              compiler-nix-name = "ghc8104";
-              shell = {
-                tools = {
-                  cabal = { };
-                  ghcid = { };
-                  haskell-language-server = { };
-                  hlint = { };
-                  ormolu = { };
-                };
-                ## ormolu that uses ImportQualifiedPost.
-                ## To use, remove ormolu from the shell.tools section above, and uncomment the following lines.
-                # buildInputs =
-                #   let
-                #     ormolu = pkgs.haskell-nix.tool compiler-nix-name "ormolu" "latest";
-                #     ormolu-wrapped = pkgs.writeShellScriptBin "ormolu" ''
-                #       ${ormolu}/bin/ormolu --ghc-opt=-XImportQualifiedPost $@
-                #     '';
-                #   in
-                #   [ ormolu-wrapped ];
-              };
-            };
         };
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
-            haskellNix.overlay
             overlay
           ];
         };
-        flake = pkgs.hsPkgs.flake { };
       in
-      flake // {
-        defaultPackage = flake.packages."purenix:exe:purenix";
-
-        devShell = flake.devShell.overrideAttrs (oldAttrs: {
-          nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [
-            pkgs.hsPkgs.hsPkgs.purescript.components.exes.purs
-            pkgs.nodejs
-            pkgs.spago
-
-            self.defaultPackage.${system}
-          ];
-        });
+      {
+        devShell = purenix.devShells.${system}.use-purenix;
       }
     );
 }
